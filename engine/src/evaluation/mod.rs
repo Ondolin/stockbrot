70d5960574b6f5a9351958ee3147daf720673cea
Ondolin <developer@ondolin.de>
2022-12-09 17:50:56 +0100

    removed useless file

--- engine/src/evaluation/mod.rs
+++ engine/src/evaluation/mod.rs
@@ -1,8 +1,7 @@
 mod evaluate;
-mod good_piece_sq_tables;
-mod simple_piece_sq_tables;
+mod piece_sq_tables;
 
 pub const MATE_SCORE: i32 = 1_000_000;
 
 pub use evaluate::evaluate;
-pub use good_piece_sq_tables::{mg_value, eg_value, game_phase_inc};
+pub use piece_sq_tables::{mg_value, eg_value, game_phase_inc};
